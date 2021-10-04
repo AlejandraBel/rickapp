30de4aa11a0cb0f9171f89aa86701a494aaecf37
AlejandraBel <91209694+AlejandraBel@users.noreply.github.com>
2021-10-04 13:49:55 -0600

    estoy en pie de pagina
    estoy en pie de pagina

---
+++ rickapp/src/components/Main.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <section className="main-section">
+        <div className="main-overlay"></div>
+
+        <div className="main-logo">
+          <img
+            src="./assets/logo-cinear-main.svg"
+            alt="logo cinear seccion principal"
+          />
+        </div>
+
+        <div className="main-gradient"></div>
+
+        <video className="main-video" autoPlay muted loop>
+          <source
+            src="https://cdn.videvo.net/videvo_files/video/premium/video0040/large_watermarked/universal_countdown1_preview.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </section>
+      <section className="extern-links-section">
+        <ul>
+          <li>
+            <a href="/">
+              <img src="<http://cine.ar/img/estrenos.cine.ar.svg>" alt="icono" />
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/">
+              <img src="http://cine.ar/img/estrenos.cine.ar.svg" alt="icono" />
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/">
+              <img src="http://cine.ar/img/estrenos.cine.ar.svg" alt="icono" />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </main> )
+    );
+  
+}
+
+export default Main;
